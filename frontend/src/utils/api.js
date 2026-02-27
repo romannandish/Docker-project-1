bd010f07e68a154d5c26f95112a581a6b999838d
romannandish <romannandish690@gmail.com>
2026-02-27 20:03:51 +0530

    fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/user',
+  baseURL: 'http://3.10.58.67:5000/api/user',
 });
 
 export default instance;
