357c4a3d6ce2c739bf302acfe346c1f0f2d086d5
romannandish <romannandish690@gmail.com>
2026-02-27 20:59:26 +0530

    fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://3.10.58.67:5000/api/user',
+  baseURL: 'http://mern-backend-container:5000/api/user',
 });
 
 export default instance;
